0fa5abaefcac43931117bbce3405d1da37c953b9
chromodyne <dsinitiere@outlook.com>
2022-08-16 15:41:48 -0700

    Basic thought routing implemented.

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,1 +1,25 @@
-//TODO
+const Thought = require("../models/Thought");
+
+module.exports = {
+
+    getThoughts(req, res) {
+        Thought.find().then((thoughts) => res.json(thoughts)).catch((err) => res.status(500).json(err));
+    },
+
+    getOneThought(req, res) {
+        Thought.findOne({ _id: req.params.thoughtId}).select("-__v")
+            .then(async (thought) => {
+                !thought ? res.status(404).json({ message: "Thought not found." })
+                    : res.json(thought)
+            }).catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+        });
+    },
+
+    createThought(req, res) {
+        Thought.create(req.body)
+            .then((thought) => res.json(thought)).catch((err) => res.status(500).json(err));
+    }
+
+}
