daebf83dc1a02d6315523d3a9c74e585766b79aa
chromodyne <dsinitiere@outlook.com>
2022-08-15 18:08:47 -0700

    Routes and basic api requests began. Getting all users now works.

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,8 @@
-//TODO
+const User = require("../models/User");
+
+module.exports = {
+    //Get all users request.
+    getUsers(req, res) {
+        User.find().then((users) => res.json(users)).catch((err) => res.status(500).json(err));
+    }
+}
--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,4 +1,7 @@
 const router = require("express").Router();
+const { getUsers } = require("../../controllers/userController.js");
 
+//TODO: Add proper routing to mongoose here.
+router.route("/").get(getUsers);
 
 module.exports = router;
